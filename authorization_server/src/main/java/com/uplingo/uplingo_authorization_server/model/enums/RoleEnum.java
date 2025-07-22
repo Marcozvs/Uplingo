26f3f00ca47eec0d3eb88207dfd83fd53902055d
Marcozvs <marcossampa.vinicius@gmail.com>
2025-07-22 19:36:27 -0400

    feat: #0002 - Defining model layer

---
+++ authorization_server/src/main/java/com/uplingo/uplingo_authorization_server/model/enums/RoleEnum.java
@@ -0,0 +1,18 @@
+package com.uplingo.uplingo_authorization_server.model.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum RoleEnum {
+  ADMIN("ADMIN"),
+  STUDENT("STUDENT");
+
+  private final String value;
+
+  @Override
+  public String toString() {
+    return value;
+  }
+}
