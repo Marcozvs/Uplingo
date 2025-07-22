4f18270edad2126638651c03db95459b6a7b2c40
Marcozvs <marcossampa.vinicius@gmail.com>
2025-07-22 19:35:36 -0400

    feat: #0002 - Starting authorization server project

---
+++ authorization_server/src/main/java/com/uplingo/uplingo_authorization_server/UplingoAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.uplingo.uplingo_authorization_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UplingoAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UplingoAuthorizationServerApplication.class, args);
+	}
+
+}
