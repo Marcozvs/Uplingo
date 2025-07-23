51e1609abbb8c8cefbc50b75c1a7f599ded56c78
Marcozvs <marcossampa.vinicius@gmail.com>
2025-07-22 20:29:55 -0400

    feat: #0003 - Starting resource server project

---
+++ resource_server/src/main/java/com/uplingo/uplingo_resource_server/UplingoResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.uplingo.uplingo_resource_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UplingoResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UplingoResourceServerApplication.class, args);
+	}
+
+}
---
+++ resource_server/src/test/java/com/uplingo/uplingo_resource_server/UplingoResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uplingo.uplingo_resource_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UplingoResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
